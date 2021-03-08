2d56a65ce4c156c3a79a233d2b84b5845c52b36a
Dave Curry <lordsnoosh@gmail.com>
2021-03-08 12:42:08 -0800

    removed old JS to make room

--- js/main.js
+++ js/main.js
@@ -153,64 +153,18 @@ function stand() {
   if (winner == null) {
     gameContent.next();
   }
-}
-function next() {}
-function dealerAI() {}
+};
 
-// const suits = ['s', 'c', 'd', 'h'];
-// const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+function next() {
+    gameContent.turn = gameContent.turn==0 ? 1 : 0;
+    if (gameContent.turn==1) {
+        if (gameContent.dealerStay) {gameContent.turn = 0; }
+        else {gameContent.dealerAi();}
+    }
+    else {
+        if (gameContent.playerstay) {gameContent.turn = 1; gameContent.dealerAi() ;}
+    }
+};
 
-// // Build a 'master' deck of 'card' objects used to create shuffled decks
-// const masterDeck = buildMasterDeck();
-// renderDeckInContainer(masterDeck, document.getElementById('holecards'));
+function dealerAi() {}
 
-// /*----- app's state (variables) -----*/
-// let shuffledDeck;
-
-// /*----- cached element references -----*/
-// const shuffledContainer = document.getElementById('holecards');
-
-// /*----- event listeners -----*/
-// document.querySelector('button').addEventListener('click', renderShuffledDeck);
-
-// /*----- functions -----*/
-// function renderShuffledDeck() {
-//   // Create a copy of the masterDeck (leave masterDeck untouched!)
-//   const tempDeck = [...masterDeck];
-//   shuffledDeck = [];
-//   while (tempDeck.length) {
-//     // Get a random index for a card still in the tempDeck
-//     const rndIdx = Math.floor(Math.random() * tempDeck.length);
-//     // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-//     shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-//   }
-//   renderDeckInContainer(shuffledDeck, shuffledContainer);
-// }
-
-// function renderDeckInContainer(deck, container) {
-//   container.innerHTML = 'null';
-//   // Let's build the cards as a string of HTML
-//   // Use reduce when you want to 'reduce' the array into a single thing - in this case a string of HTML markup
-//   const cardsHtml = deck.reduce(function(src, card) {
-//     return src + `<div class="card ${card.face}"></div>`;
-//   }, '');
-//   container.innerHTML = cardsHtml;
-// }
-
-// function buildMasterDeck() {
-//   const deck = [];
-//   // Use nested forEach to generate card objects
-//   suits.forEach(function(suit) {
-//     ranks.forEach(function(rank) {
-//       deck.push({
-//         // The 'face' property maps to the library's CSS classes for cards
-//         face: `${suit}${rank}`,
-//         // Setting the 'value' property for game of blackjack, not war
-//         value: Number(rank) || (rank === 'A' ? 11 : 10)
-//       });
-//     });
-//   });
-//   return deck;
-// }
-
-// renderShuffledDeck();
